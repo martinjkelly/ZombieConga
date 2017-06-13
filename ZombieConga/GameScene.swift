6bf867434ecd892da56044406da3dd40641365e5
Martin Kelly <marty.kelly2009@gmail.com>
2017-06-13 14:23:08 +0100

    Updated move implementation. Now on the end of page 68

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,7 +11,12 @@ import GameplayKit
 
 class GameScene: SKScene {
 
+    let zombieMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPoint.zero
+    
     let zombie = SKSpriteNode(imageNamed: "zombie1")
+    var lastUpdateTime: TimeInterval = 0
+    var dt: TimeInterval = 0
     
     override func didMove(to view: SKView) {
         backgroundColor = .black
@@ -22,5 +27,65 @@ class GameScene: SKScene {
         
         zombie.position = CGPoint(x: 400, y: 400)
         addChild(zombie)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        print("\(dt*1000) milliseconds since last update")
+        
+        move(sprite: zombie, velocity: velocity)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLocation: touchLocation)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLocation: touchLocation)
+    }
+    
+    func sceneTouched(touchLocation: CGPoint) {
+        moveZombieToward(location: touchLocation)
+    }
+    
+    func move(sprite: SKSpriteNode, velocity: CGPoint) {
+        
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt),
+                                   y: velocity.y * CGFloat(dt))
+        print("Amount to move: '\(amountToMove)")
+        
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x,
+                                  y: sprite.position.y + amountToMove.y)
+    }
+    
+    func moveZombieToward(location: CGPoint) {
+        
+        let offset = CGPoint(x: location.x - zombie.position.x,
+                             y: location.y - zombie.position.y)
+        
+        let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
+        
+        let direction = CGPoint(x: offset.x / CGFloat(length),
+                                y: offset.y / CGFloat(length))
+        
+        velocity = CGPoint(x: direction.x * zombieMovePointsPerSec,
+                           y: direction.y * zombieMovePointsPerSec)
+        
     }
 }
